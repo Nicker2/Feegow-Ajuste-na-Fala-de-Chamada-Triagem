e28cb9b140fdd889da2b66e965e27adff1c75f53
Nicolas <34519753+Nicker2@users.noreply.github.com>
2025-10-08 20:16:42 -0300

    Create Feegow Triagem - Interceptar e Modificar Fala.meta.js

---
+++ Feegow Triagem - Interceptar e Modificar Fala.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Interceptar e Modificar Fala e Texto na Tela (Atualizado com Nomes e Diferenciação de Setores) - Otimizado
+// @namespace    http://tampermonkey.net/
+// @version      2.2.5
+// @description  Modifica a fala e o texto na tela, diferenciando chamadas de pré-consulta (Sala de Pré-Consulta) e exames (Central de Diagnósticos), com botão de tela cheia no canto superior esquerdo.
+// @match        https://core.feegow.com/tvcall/panelV3/vvAM/*
+// @match        https://core.feegow.com/tvcall/panelV3/pzMY/7
+// @downloadURL  https://github.com/Nicker2/Feegow-Ajuste-na-Fala-de-Chamada-Triagem/raw/refs/heads/main/Feegow%20Triagem%20-%20Interceptar%20e%20Modificar%20Fala.user.js
+// @updateURL    https://github.com/Nicker2/Feegow-Ajuste-na-Fala-de-Chamada-Triagem/raw/refs/heads/main/Feegow%20Triagem%20-%20Interceptar%20e%20Modificar%20Fala.meta.js
+// @icon         https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://app.feegow.com/&size=16
+// @grant        none
+// ==/UserScript==
